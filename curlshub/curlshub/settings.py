ccfcd6b5304d2da35de5534fcffb26efc70dcb83
irfan <irfan7861@live.com>
2021-06-15 00:08:23 +0530

    database

--- curlshub/curlshub/settings.py
+++ curlshub/curlshub/settings.py
@@ -32,8 +32,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +122,5 @@ STATICFILES_DIRS = (
 )
 
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
